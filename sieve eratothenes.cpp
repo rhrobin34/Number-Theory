e0468069d22f63c87dc52fdb0206dfb844c3c805
rhrobin43 <rhrobin439@gmail.com>
2024-01-12 15:50:38 +0600

    Number Theory

--- sieve eratothenes.cpp
+++ sieve eratothenes.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 int main()
 {
-    
+     int t;
+     cin>>t;
+     while(t--)
+     {
+        int n;
+        cin>>n;
+        int arr[n+3];
+        memset(arr,0,sizeof(arr));
+        for(int i=2;i*i<=n;i++)
+        {
+            if(arr[i]==0)
+            {
+                for(int j=i*i;j<=n;j+=i)
+                {
+                    arr[j]=1;
+                }
+            }
+        }
+        for(int i=2;i<=n;i++)
+        {
+            if(arr[i]==0)
+            {
+                cout<<i<<" ";
+            }
+        }
+        cout<<endl;
+     }
 }
